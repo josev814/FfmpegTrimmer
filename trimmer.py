e560c692a91800690add8dd1440907ee9e9b3666
josev814 <josev814@gmail.com>
2025-02-02 22:35:57 -0500

    addin pyzip as a requirement
    updating the trimmer

--- trimmer.py
+++ trimmer.py
@@ -1,6 +1,9 @@
+import shutil
+import json
 import sys
 import subprocess
 import os
+import glob
 import ffmpeg
 import numpy as np
 from PyQt6.QtWidgets import (
@@ -9,32 +12,92 @@ from PyQt6.QtWidgets import (
 )
 from PyQt6.QtCore import QThread, pyqtSignal, Qt
 import requests
+from py7zip import py7zip
 
 # FFmpeg download URL (Windows version)
-FFMPEG_URL = "https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-i686-static.tar.xz"
+FFMPEG_URL = "https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z"
 FFMPEG_DIR = "ffmpeg"
+VLC_WINDOWS_URL = 'https://download.videolan.org/pub/videolan/vlc/last/win64/vlc-3.0.20-win64.exe'
+VLC_PATH = r'C:\Program Files\VideoLAN\VLC'
 # possibly switch to github builds
 # https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n7.1-latest-win64-gpl-7.1.zip
 
-def download_ffmpeg():
-    """Downloads FFmpeg if not found in the current directory."""
-    if not os.path.exists(FFMPEG_DIR):
-        print("FFmpeg not found. Downloading...")
-        response = requests.get(FFMPEG_URL, stream=True)
-        with open("ffmpeg-release-i686-static.tar.xz", "wb") as file:
+class Install_Requirements():
+
+    def __init__(self):
+        if not self.is_vlc_installed():
+            self.install_vlc()
+
+    def is_vlc_installed(self):
+        if os.path.isfile(os.path.join(VLC_PATH, 'vlc.exe')):
+            return True
+        return False
+
+    def download_vlc(self, installer_path):
+        print('VLC is not installed. Downloading...')
+        self.download_file(VLC_WINDOWS_URL, installer_path)
+
+    def download_file(self, url, dest):
+        response = requests.get(url, stream=True)
+        with open(dest, 'wb') as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
-        
-        # Extract the tar file
-        import tarfile
-        with tarfile.open("ffmpeg-release-i686-static.tar.xz", "r:xz") as tar:
-            tar.extractall()
-        
-        exes = ['ffmpeg', 'ffplay', 'ffprob']
 
-        for exe in exes:
-            os.rename(f"ffmpeg-*-static/{exe}.exe", f"{exe}.exe")
-        os.rmdir("ffmpeg-*-static")
+    def install_vlc(self):
+        installer_path = 'vlc_installer.exe'
+        if not os.path.isfile(installer_path):
+            self.download_vlc(installer_path)
+        print('Installing VLC')
+        subprocess.run([installer_path, '/S'], shell=True)
+        os.remove(installer_path)
+        print('VLC install Complete')
+
+    def extract_ffmpeg(self, build_file):
+        # Extract the tar file
+        py7 = py7zip.Py7zip()
+        bin_url = py7.get_binary_url()
+        # Download and set up the binary if it isn't already set up
+        if not os.path.exists(py7.binary_path):
+            py7.download_binary()
+            py7.setup()
+        dst=f'./'
+        cmd = [py7.binary_path, 'x', build_file, f'-bb3', f'-o{dst}']
+
+        # Run the subprocess and capture output
+        try:
+            result = subprocess.run(cmd, check=False, text=False, capture_output=False)
+            print(f"Decompression completed successfully! Output:\n{result.stdout}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error during decompression: {e.stderr}")
+        os.remove(build_file)
+
+    def download_ffmpeg(self, build_file):
+        """Downloads FFmpeg if not found in the current directory."""
+        if os.path.exists(FFMPEG_DIR) and os.path.isfile(os.path.join(FFMPEG_DIR, 'ffmpeg.exe')):
+            return
+        if os.path.isfile(build_file):
+            # release already downloaded
+            return
+        print("Downloading FFmpeg...")
+        self.download_file(FFMPEG_URL, build_file)
+
+    def install_ffmpeg(self):
+        build_file = 'ffmpeg-release-full.7z'
+        self.download_ffmpeg(build_file)
+        if os.path.isfile(build_file):
+            extract_ffmpeg(build_file)
+        else:
+            raise FileNotFoundError(f'Failed to find {build_file}')
+
+        exes = ['ffmpeg', 'ffplay', 'ffprobe']
+        for ffmpeg_dir in glob.glob('ffmpeg-*-full_build'):
+            for entry in os.scandir(os.path.join(ffmpeg_dir, 'bin')):
+                if entry.name.endswith('exe') and entry.name.replace('.exe', '') in exes and not os.path.isfile(f'./{FFMPEG_DIR}/{entry.name}'):
+                    os.rename(entry.path, f'./{FFMPEG_DIR}/{entry.name}')
+                    continue
+        for entry in os.scandir('./'):
+            if entry.is_dir() and entry.name.endswith('full_build'):
+                shutil.rmtree(entry.path)
 
 
 class VideoCutterThread(QThread):
@@ -58,12 +121,12 @@ class VideoCutterThread(QThread):
         )
 
         command = [
-            "ffmpeg",
+            f"{FFMPEG_DIR}/ffmpeg.exe",
             "-i", self.file_path,
             "-ss", self.start_time,
             "-to", self.end_time,
             "-c:v", "copy",
-            "-c:a", "aac"
+            "-c:a", "aac",
         ]
 
         if self.audio_level in ["-1", "-3", "-5"]:
@@ -199,18 +262,29 @@ class VideoCutterApp(QWidget):
     def update_video_duration(self, file_path):
         """Retrieves and displays the video duration."""
         try:
-            probe = ffmpeg.probe(file_path)
-            duration = float(probe["format"]["duration"])
+            # Run ffprobe to get video metadata in JSON format
+            cmd = [f'{FFMPEG_DIR}/ffprobe.exe', '-v', 'error', '-show_entries', 'format=duration', '-of', 'json', file_path]
+            result = subprocess.run(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+
+            # Parse the JSON output from ffprobe
+            probe_output = json.loads(result.stdout)
+            duration = float(probe_output['format']['duration'])
+
+            # Convert duration to hh:mm:ss format
             hh, mm, ss = int(duration // 3600), int((duration % 3600) // 60), int(duration % 60)
             self.label_duration.setText(f"Duration: {hh:02}:{mm:02}:{ss:02}")
-        except Exception:
+            self.input_end.setText(f"{hh:02}:{mm:02}:{ss:02}")
+            self.input_start.setText("00:00:00")
+
+        except Exception as e:
             self.label_duration.setText("Duration: Error reading")
+            print(f"Error: {e}")
 
     def analyze_audio_level(self, file_path):
         """Analyzes the current audio dB level of the video."""
         try:
             result = subprocess.run(
-                ["ffmpeg", "-i", file_path, "-af", "volumedetect", "-f", "null", "-"],
+                [f"{FFMPEG_DIR}/ffmpeg.exe", "-i", file_path, "-af", "volumedetect", "-f", "null", "-"],
                 stderr=subprocess.PIPE,
                 text=True
             )
@@ -233,14 +307,63 @@ class VideoCutterApp(QWidget):
             QMessageBox.critical(self, "Error", "Please select a file and enter start/end times.")
             return
 
-        command = ["ffplay", "-i", file_path, "-ss", start_time, "-to", end_time]
-        subprocess.Popen(command)
+        # Convert start_time and end_time to seconds (useful for calculating duration)
+        start_time_seconds = sum(x * int(t) for x, t in zip([3600, 60, 1], start_time.split(":")))
+        end_time_seconds = sum(x * int(t) for x, t in zip([3600, 60, 1], end_time.split(":")))
+
+        # Calculate the duration to play
+        duration = end_time_seconds - start_time_seconds
+
+        # try:
+        #     command = [f"{FFMPEG_DIR}/ffplay.exe", "-i", file_path, "-ss", start_time, "-t", f'{duration}']
+        #     process = subprocess.Popen(command)
+        #     stdout, stderr = process.communicate()
+        # except Exception as e:
+        #     print(e)
+        vlc_command = [
+            os.path.join(VLC_PATH, 'vlc.exe'), f'file:///{file_path}',
+            f'--start-time={start_time_seconds}',
+            f'--stop-time={end_time_seconds}',
+            '--play-and-exit'
+        ]
+        try:
+            subprocess.Popen(vlc_command)
+        except FileNotFoundError:
+            QMessageBox.critical(
+                self,
+                'Error',
+                'VLC installation failed'
+            )
 
     def select_output_folder(self):
         """Opens a folder dialog for selecting an output folder."""
         folder_path = QFileDialog.getExistingDirectory(self, "Select Output Folder")
         if folder_path:
             self.input_output.setText(folder_path)
+
+    def validate_inputs(self):
+        valid = True
+        file_path = self.input_file.text()
+        start_time = self.input_start.text()
+        end_time = self.input_end.text()
+        output_path = self.input_output.text()
+
+        if not file_path or not start_time or not end_time or not output_path:
+            QMessageBox.critical(self, "Error", "Please complete all fields.")
+            valid = False
+
+        if not re.match(r'^[0-9]+:[0-5][0-9]:[0-5][0-9]$', start_time):
+            QMessageBox.critical(self, "Error", "Invalid Start Time Format.")
+            valid = False
+
+        if not re.match(r'^[0-9]+:[0-5][0-9]:[0-5][0-9]$', end_time):
+            QMessageBox.critical(self, "Error", "Invalid End Time Format.")
+            valid = False
+
+        if not os.path.isdir(output_path):
+            QMessageBox.critical(self, "Error", "Output Path doesn't exist.")
+            valid = False
+        return valid
 
     def extract_video(self):
         """Extracts a video segment based on user input."""
@@ -250,8 +373,7 @@ class VideoCutterApp(QWidget):
         output_path = self.input_output.text()
         audio_level = self.audio_options.currentText().split()[0]
 
-        if not file_path or not start_time or not end_time or not output_path:
-            QMessageBox.critical(self, "Error", "Please complete all fields.")
+        if not self.validate_inputs():
             return
 
         self.progress_bar.setValue(0)
@@ -267,7 +389,7 @@ class VideoCutterApp(QWidget):
 
 
 if __name__ == "__main__":
-    download_ffmpeg()
+    Install_Requirements()
     app = QApplication(sys.argv)
     window = VideoCutterApp()
     window.show()
